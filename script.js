d9859ce14b361acbe3758ea62c6c3bc8726345d8
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-11 08:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
   $(document).ready(function() {
       $('#weatherButton').click(function() {
         // Make a GET request to OpenWeatherMap API
-        $.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY', function(data) {
+        $.get('https://api.openweathermap.org/data/2.5/weather?q=London&appid=', function(data) {
           // Extract the weather description from the API response
-          var weatherDescription = data.weather[0].description;
+          let weatherDescription = data.weather[0].description;
           // Update the weatherData div with the weather information
           $('#weatherData').text('Current weather in London: ' + weatherDescription);
         });
